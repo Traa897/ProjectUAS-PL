f1e96bd265dfa5a6edc498c3b0b5cea02cc56c67
Traa897 <patran05534@gmail.com>
2025-11-15 17:47:20 +0800

    update header

--- P!X/views/layouts/header.php
+++ P!X/views/layouts/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>P! XMovie</title>
+    <title>P!X Movie</title>
     <link rel="stylesheet" href="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/assets/style.css">
 </head>
 <body>
